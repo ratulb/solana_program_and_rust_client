e363976da2dd2eef72280a9e79a5b654edfd066e
Ratul Buragohain <ratul75@hotmail.com>
2022-04-16 14:31:52 +0000

    Error handling note

--- client/src/errors.rs
+++ client/src/errors.rs
@@ -1,1 +1,4 @@
+//Our error handling is not robust - that is intentional
+//We care more about descriptive error messages that we 
+//may encounter rather than handling them in this learning exercise
 pub type Result<T> = std::result::Result<T, String>;
